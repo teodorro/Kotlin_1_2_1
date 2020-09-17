77f2a90a1babd78e184d171f410a1964ef0335a1
teodorro <edem.teo@gmail.com>
2020-09-17 19:11:51 +0300

    1.4.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
-fun main(){
-    val minCommission = 35*100;
-    val percentCommission = 0.0075;
 
+const val minCommission = 35*100;
+const val percentCommission = 0.0075;
+
+
+fun main(){
     val amount1 = 1000000;
     val amount2 = 10000;
 
-    val commission1 = if (amount1 * percentCommission > minCommission) (amount1 * percentCommission).toInt() else minCommission;
-    println("Сумма: $amount1 коп; Комиссия: $commission1 коп");
-    val commission2 = if (amount2 * percentCommission > minCommission) (amount2 * percentCommission).toInt() else minCommission;
-    println("Сумма: $amount2 коп; Комиссия: $commission2 коп");
-
+    print(amount1);
+    print(amount2);
 }
 
+fun print(amount: Int){
+    val commission1 = if (amount * percentCommission > minCommission) (amount * percentCommission).toInt() else minCommission;
+    println("Сумма: $amount коп; Комиссия: $commission1 коп");
+}
